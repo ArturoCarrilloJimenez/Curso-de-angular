d2d34e3e9bc6cecbbb1cb30a39cf515ec2c640d8
arturillo40 <arturojimenezalmontaras@gmail.com>
2024-11-24 13:04:03 +0100

    mejora de la búsqueda por regiones

--- countryApp/src/app/country/pages/by-region-page/by-region-page.component.ts
+++ countryApp/src/app/country/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/conuntries.service';
 
+type Region = 'Africa' | 'Asia' | 'America' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,15 +11,26 @@ import { CountryService } from '../../services/conuntries.service';
 })
 export class ByRegionPageComponent {
   public regions: Country[] = [];
-  public isLoading = false
+
+  public regionsValid: Region[] = [
+    'Africa',
+    'Asia',
+    'America',
+    'Europe',
+    'Oceania',
+  ];
+  public selectedRegion?: Region;
+
+  public isLoading = false;
 
   constructor(private countryService: CountryService) {}
 
-  searchByRegion(text: string) {
-    this.isLoading = true
-    this.countryService.searchRegion(text).subscribe((regions) =>  {
-      this.regions = regions
-      this.isLoading = false
-    })
+  searchByRegion(text: Region) {
+    this.isLoading = true;
+    this.selectedRegion = text
+    this.countryService.searchRegion(text).subscribe((regions) => {
+      this.regions = regions;
+      this.isLoading = false;
+    });
   }
 }
