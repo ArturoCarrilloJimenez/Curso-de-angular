6423b7da3cdde1bf09b61c164401cfbcaec2d110
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-03-11 13:22:34 +0100

    validación de forma reactiva

--- 05-reactiveFormApp/src/app/rective/page/basic-page/basic-page.component.ts
+++ 05-reactiveFormApp/src/app/rective/page/basic-page/basic-page.component.ts
@@ -1,6 +1,12 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  isFormArray,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -17,9 +23,50 @@ export class BasicPageComponent {
 
   formBider = inject(FormBuilder);
 
-  myForm = this.formBider.group({
-    name: ['', [Validators.required,Validators.minLength(3)]],
+  myForm: FormGroup = this.formBider.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
     price: [0, [Validators.required, Validators.min(10)]],
     inStorage: [0, [Validators.required, Validators.min(0)]],
   });
+
+  isValidField(fielName: string): boolean | null {
+    return (
+      this.myForm.controls[fielName].errors &&
+      this.myForm.controls[fielName].touched
+    );
+  }
+
+  getFielError(fielName: string): string | null {
+    if (!this.myForm.controls[fielName]) return null;
+
+    // Obtengo los errores
+    const errors = this.myForm.controls[fielName].errors ?? {};
+
+    for (let key of Object.keys(errors)) {
+      // Según el error muestro un mensaje distinto
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
+
+        case 'min':
+          return `Debe ser mayor de ${errors['min'].min}`;
+      }
+    }
+
+    return null;
+  }
+
+  onSave() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched(); // Toca todos los campos para mostrar la validación
+      return;
+    }
+
+    console.log(this.myForm.value);
+
+    this.myForm.reset(); // Respeta el form
+  }
 }
