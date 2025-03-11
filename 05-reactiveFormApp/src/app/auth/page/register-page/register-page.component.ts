78558c47677da8ddd12d1413d46b8180c71befa3
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-03-11 18:13:46 +0100

    validación asíncrona y personalizada

--- 05-reactiveFormApp/src/app/auth/page/register-page/register-page.component.ts
+++ 05-reactiveFormApp/src/app/auth/page/register-page/register-page.component.ts
@@ -1,12 +1,54 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-register-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './register-page.component.html',
-  styleUrl: './register-page.component.scss'
+  styleUrl: './register-page.component.scss',
 })
 export class RegisterPageComponent {
+  private fb = inject(FormBuilder);
 
+  formUtils = FormUtils;
+
+  myForm: FormGroup = this.fb.group(
+    {
+      name: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.namePattern)],
+      ],
+      email: [
+        '',
+        [Validators.required, Validators.pattern(this.formUtils.emailPattern)],
+        [FormUtils.checkingServerResponse]
+      ],
+      username: ['', [Validators.required, Validators.minLength(6), this.formUtils.checkingStrider]],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern(this.formUtils.notOnlySpacesPattern),
+        ],
+      ],
+      confirmPassword: ['', [Validators.required]],
+    },
+    {
+      validators: [
+        this.formUtils.isFielOneEqualFielTwo('password', 'confirmPassword')
+      ],
+    }
+  );
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }
--- 05-reactiveFormApp/src/app/utils/form-utils.ts
+++ 05-reactiveFormApp/src/app/utils/form-utils.ts
@@ -1,6 +1,25 @@
-import { FormArray, FormGroup, ValidationErrors } from '@angular/forms';
+import {
+  AbstractControl,
+  FormArray,
+  FormGroup,
+  ValidationErrors,
+} from '@angular/forms';
+
+async function diley(ms: number) {
+  return new Promise((res) => {
+    setTimeout(() => {
+      res(true);
+    }, ms);
+  });
+}
 
 export class FormUtils {
+  // Expresiones regulares
+  static namePattern = '([a-zA-Z]+) ([a-zA-Z]+)';
+  static emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
+  static notOnlySpacesPattern = '^[a-zA-Z0-9]+$';
+
+  // Errores personalizados
   static errorController(errors: ValidationErrors): string | null {
     for (let key of Object.keys(errors)) {
       // Según el error muestro un mensaje distinto
@@ -13,12 +32,38 @@ export class FormUtils {
 
         case 'min':
           return `Debe ser mayor de ${errors['min'].min}`;
+
+        case 'email':
+          return `Este correo no es valido`;
+
+        case 'pattern':
+          // Comprobación de los patrones
+          switch (errors['pattern'].requiredPattern) {
+            case this.namePattern:
+              return 'Debes de introducir nombre y primer apellido';
+
+            case this.emailPattern:
+              return 'Email incorrecto, comprueba que este escrito de forma correcta';
+
+            case this.notOnlySpacesPattern:
+              return 'Este campo no debe de tener espacios';
+
+            default:
+              return 'Error con la comprobación de patrones';
+          }
+
+        case 'emailTaken':
+          return 'Este correo ya esta utilizado';
+
+        default:
+          return 'Error de validación no comprobada';
       }
     }
 
     return null;
   }
 
+  // Validaciones
   static isValidField(form: FormGroup, fielName: string): boolean | null {
     return form.controls[fielName].errors && form.controls[fielName].touched;
   }
@@ -45,5 +90,35 @@ export class FormUtils {
     const errors = form.controls[index].errors ?? {};
 
     return this.errorController(errors);
+  }
+
+  // Validador personalizado
+  static isFielOneEqualFielTwo(fiel1: string, fiel2: string) {
+    return (formGroup: AbstractControl) => {
+      const fiel1Value = formGroup.get(fiel1)?.value;
+      const fiel2Value = formGroup.get(fiel2)?.value;
+
+      return fiel1 === fiel2 ? null : { passwordNotEqual: true };
+    };
+  }
+
+  static async checkingServerResponse(
+    control: AbstractControl
+  ): Promise<ValidationErrors | null> {
+    console.log('Llamada al server');
+
+    await diley(1500);
+
+    const fromValue = control.value;
+
+    return fromValue === 'admin@admin.com' ? { emailTaken: true } : null;
+  }
+
+  static checkingStrider(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    const fromValue = control.value;
+
+    return fromValue === 'strider' ? { notStrider: true } : null;
   }
 }
