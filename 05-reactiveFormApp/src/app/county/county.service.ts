f5a9f96a66437948d7cecd38caf1ed1ffa4c16ec
ArturoCarrilloJimenez <arturojimenezalmontaras@gmail.com>
2025-03-12 11:51:48 +0100

    fin formularios rectivos, el ultimo no funciona muy bien por un fallo con la optencion de los datos de los datos, ya lo solucionare en un futuro

--- 05-reactiveFormApp/src/app/county/county.service.ts
+++ 05-reactiveFormApp/src/app/county/county.service.ts
@@ -1,9 +1,49 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { combineLatest, Observable, of } from 'rxjs';
+import { Country } from './interface/country.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountyService {
+  private base = 'https://restcountries.com/v3.1';
+  private http = inject(HttpClient);
 
-  constructor() { }
+  constructor() {}
+
+  private _region = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  public get region(): string[] {
+    return [...this._region];
+  }
+
+  getCountriesByRegion(region: string): Observable<Country[]> {
+    if (!region) return of([]);
+
+    const url = `${this.base}/region/${region}`;
+    return this.http.get<Country[]>(url);
+  }
+
+  getCountryByAlphaCode(alfaCode: string): Observable<Country[]> {
+    const url = `${this.base}/alpha/${alfaCode}`;
+    return this.http.get<Country[]>(url);
+  }
+
+  // TODO falla la busqueda de pases fronterizos
+  getCountryNamesByCodeArray(countryCodes: string[]): Observable<Country[]> {
+    if (!countryCodes || countryCodes.length === 0) return of([]);
+
+    const countriesRequest: Observable<Country[]>[] = [];
+
+    countryCodes.map((code) => {
+      const request = this.getCountryByAlphaCode(code);
+      countriesRequest.push(request)
+    });
+
+    // Funcion que lo combina todo en un observable
+    console.log(combineLatest(countriesRequest).subscribe(console.log));
+
+    return of([]);
+  }
 }
--- 05-reactiveFormApp/src/app/county/page/country-page/country-page.component.ts
+++ 05-reactiveFormApp/src/app/county/page/country-page/country-page.component.ts
@@ -1,11 +1,87 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  effect,
+  inject,
+  OnChanges,
+  signal,
+  SimpleChanges,
+} from '@angular/core';
+import { CountyService } from '../../county.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Country } from '../../interface/country.interface';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './country-page.component.html',
-  styleUrl: './country-page.component.scss'
+  styleUrl: './country-page.component.scss',
 })
 export class CountryPageComponent {
+  private fb = inject(FormBuilder);
+  private countryService = inject(CountyService);
 
+  regions = signal(this.countryService.region);
+  countryByRegion = signal<Country[]>([]);
+  border = signal<Country[]>([]);
+
+  myForm: FormGroup = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+  onFormChange = effect((onCleanUp) => {
+    const formRegionChanged = this.getRegionChanged();
+    const formCountryChanged = this.getCountryChanged();
+
+    onCleanUp(() => {
+      formRegionChanged?.unsubscribe();
+    });
+  });
+
+  getRegionChanged() {
+    return this.myForm
+      .get('region')
+      ?.valueChanges.pipe(
+        tap(() => this.myForm.controls['country'].setValue('')),
+        tap(() => this.myForm.controls['border'].setValue(''))
+      )
+      .subscribe((value: string) => {
+        value !== ''
+          ? this.countryService
+              .getCountriesByRegion(value)
+              .subscribe((element) => {
+                this.countryByRegion.set(element);
+                console.log(element);
+              })
+          : null;
+        console.log(this.countryByRegion());
+      });
+  }
+
+  getCountryChanged() {
+    return this.myForm
+      .get('country')
+      ?.valueChanges.subscribe((value: string) => {
+        value !== ''
+          ? this.countryService
+              .getCountryByAlphaCode(value)
+              .subscribe((element) => {
+                this.countryService.getCountryNamesByCodeArray(
+                  element[0].borders
+                );
+              })
+          : null;
+          console.log('border');
+
+        console.log(this.border());
+      });
+  }
 }
